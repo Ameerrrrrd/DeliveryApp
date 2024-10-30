a05a573b4923867f9a9b7a84e0a075bff9625eaa
Ameerrrrrd <tutameer@gmail.com>
2024-10-30 17:28:17 +0300

    app

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03c157cb-bc94-4d5b-9cc0-47e035c2922c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using test;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void CheckConnectionWithDataBase()
+        {
+            using (var connection = new MySqlConnection("server=127.0.0.1; user=root; database=orders; password="))
+            {
+                connection.Open();
+                Assert.AreEqual(ConnectionState.Open, connection.State);
+            }
+        }
+
+
+        private string logFilePath = "testloging.txt";
+        [TestMethod]
+
+        public void TestLoggingFunction()
+        {
+            var delivery = new Delivery();
+
+            string testLogMessage = "This a test logging message!";
+            delivery.Log(testLogMessage);
+
+            string logContent = File.ReadAllText(logFilePath);
+            StringAssert.Contains(testLogMessage, logContent, "Сообщение не записано в лог");
+
+            var lines = File.ReadAllLines(logFilePath).ToList();
+
+            lines.RemoveAll(line => line.Contains(testLogMessage));
+            File.WriteAllLines(logFilePath, lines);
+        }
+
+
+    }
+}
